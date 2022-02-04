2cab77fb525f9d145ca61fb807a00e0841e1f532
Piiman <palomar.miguel2d@gmail.com>
2022-02-03 18:32:50 -0600

    Router configurado, con 2 paginas iniciales

---
+++ src/pages/login.js
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div>
+      Hola soy un login (mundo) en una intranet
+      #TODO
+      En esta pagina va el logo con 2 campos de texto y un boton para hacer login
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/register.jsx
@@ -0,0 +1,10 @@
+function registro() {
+    return (
+      <div>
+        #TODO
+        Formulario para guardar gustos de usuario
+      </div>
+    );
+  }
+  
+  export default registro;
